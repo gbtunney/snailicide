39b71eb4e4a141fa64832e2c8caa31a43c7601a3
Gillian Tunney <gbtunney@mac.com>
2021-12-19 18:38:56 -0500

    documentation for transform string

--- packages/g-library/src/scripts/transformString/_transformString.ts
+++ packages/g-library/src/scripts/transformString/_transformString.ts
@@ -16,7 +16,7 @@ import {
     underscore,
     removeNonWord
 } from './../string'
-import {cleanIntegerType} from "@/scripts/_type";
+import {cleanBooleanType, cleanIntegerType} from "@/scripts/_type";
 
 
 export const TRANSFORM_FUNCTIONS = {
@@ -24,6 +24,7 @@ export const TRANSFORM_FUNCTIONS = {
     clean: replaceCharacters,
     stringToInt: cleanIntegerType,
     parseInt: cleanIntegerType,
+    parseBool: cleanBooleanType,
     case_sensitive: lowerCase,
     lowerCase,
     upperCase,
@@ -38,10 +39,21 @@ export const TRANSFORM_FUNCTIONS = {
     underscore,
     removeNonWord
 }
+/**
+ * transformString
+ * @param {boolean | string | number} value - single value
+ * @param {boolean|string|array<string>} [d=[' ']] blacklist
+ * @param {operations}[d=['["trim", "clean","stringToInt"]']] operations list
+ * *@param {object} [o] funcLookup - this is a object of functions ( for internal use )
+ * @return {boolean | string | number} returns transformed string.
+ * @example
+ *      transformString("spynx sucks", [" "],["sentenceCase", "clean"])
+ *      =>"Spynxsucks"
+ */
 
 export const transformString = function (value: boolean | string | number = true,
                                          blacklist: boolean | string | Array<string | boolean> = [' '],
-                                         operations: any = ["trim", "clean"],
+                                         operations: any = ["trim", "clean", "stringToInt"],
                                          funcLookup: any = TRANSFORM_FUNCTIONS) {
     if (RA.isNotString(value) || R.isEmpty(operations)) return value
     let operationsObj = {}
@@ -65,6 +77,15 @@ export const transformString = function (value: boolean | string | number = true
     return RESULT
 }
 
+/**
+ * transformStringAll - Transforms a batch of strings
+ * @param {Array| Object | boolean | string | number} value - single value
+ * @param {*}  args - parses values along to transform string
+ * @return {Array| Object  | boolean | string | number} returns batch ( or single ) transformed string.
+ * @example
+ *      transformString("spynx sucks", [" "],["sentenceCase", "clean"])
+ *      =>"Spynxsucks"
+ */
 export function transformStringAll(value: any = true,
                                    ...args: any) {
     if (RA.isBoolean(value) || R.isEmpty(value)) return false
