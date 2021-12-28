f0c3184130ea0493ca88035086ce3b8734e87b02
Gillian Tunney <gbtunney@mac.com>
2021-12-27 20:05:40 -0500

    starting pt

---
+++ apps/o-wool-2022/src/vue/components/renderless/my-component.vue
@@ -0,0 +1,15 @@
+<script>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+
+export default {
+  setup(props, { slots }) {
+    const store = useStore()
+
+    return () => slots.default({
+      visible: computed(() => store.state['my-module'].visible).value,
+      toggle: () => store.dispatch('my-module/toggle')
+    })
+  }
+}
+</script>
