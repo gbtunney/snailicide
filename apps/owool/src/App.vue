ac56d9476a0146139f238ded092d8cb31a1e820c
Gillian Tunney <gbtunney@mac.com>
2021-12-30 06:31:28 -0500

    working!!! yay!!!

--- apps/owool/src/App.vue
+++ apps/owool/src/App.vue
@@ -8,7 +8,6 @@
 <script lang="ts">
 import Vue from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
-
 export default Vue.extend({
   name: 'App',
   components: {
--- apps/owool/vue.config.js
+++ apps/owool/vue.config.js
@@ -1,4 +1,39 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
-    // options...
-    filenameHashing:false
-}
+  // options...
+  filenameHashing: false,
+  runtimeCompiler: true,
+  css: {
+    extract: true,
+  },
+  configureWebpack: {
+    plugins: [
+      new CopyPlugin({
+        //
+        patterns: [
+          {
+            from: path.resolve(__dirname, "dist/css/app.css"),
+            to: path.resolve(__dirname, "shopify/assets/"),
+          },
+          {
+            from: path.resolve(__dirname, "dist/js/app.js"),
+            to: path.resolve(__dirname, "shopify/assets/"),
+          },
+          {
+            from: path.resolve(__dirname, "src/assets/svg/"),
+            to: path.resolve(__dirname, "shopify/assets/"),
+          },
+          {
+            from: path.resolve(__dirname, "src/assets/fonts/"),
+            to: path.resolve(__dirname, "shopify/assets/"),
+          },
+          {
+            from: path.resolve(__dirname, "src/assets/brooklyn_assets/"),
+            to: path.resolve(__dirname, "shopify/assets/"),
+          },
+        ],
+      }),
+    ],
+  },
+};
