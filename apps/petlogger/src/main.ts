aaf4fb6a690ae237a2d08a480f10cb5fc503329d
Gillian Tunney <gbtunney@mac.com>
2022-03-18 19:55:54 -0400

    new files

--- apps/petlogger/src/main.ts
+++ apps/petlogger/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
+import 'windi.css'
 
 createApp(App).use(router).mount('#app')
